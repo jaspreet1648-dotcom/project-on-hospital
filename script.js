cba3ed7db1f1007f4e90befef79d5a9958b288ed
jaspreet1648-dotcom <redevi2344@gmail.com>
2026-05-22 16:48:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+
+function sendMsg() {
+    alert("Message sent successfully!");
+}
+
+// Active navbar link on scroll
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - 100;
+        const sectionHeight = section.clientHeight;
+
+        if (pageYOffset >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+        if (link.getAttribute("href").includes(current)) {
+            link.classList.add("active");
+        }
+    });
+});
